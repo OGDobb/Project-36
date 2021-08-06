bf919fac0f10c5c3d02330c3c955529875b86e37
OGDobb <73088984+OGDobb@users.noreply.github.com>
2021-08-05 20:44:42 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -42,9 +42,6 @@ function setup() {
 
 function draw() {  
 background(46,139,87);
-//if(keyWentDown(UP_ARROW)){
-//writeStock(foodS);
-//dog.addImage(happyDog);
 food = new Food();
 textSize(15);
 fill("white");
